6d39c9a5533d60ab254d439943345966e65ac93c
thisIsAniketNare <aniket.nare98@gmail.com>
2016-12-21 15:37:12 +0530

    Space Animation
    A simple rotation of two GameObjects, one(earth) around sun, and
    other(moon) around earth making moon as a child of earth.

---
+++ Assets/Rotate.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Rotate : MonoBehaviour {
+
+	public int speed;
+	public int x;
+	public int y;
+	public int z;
+	// Update is called once per frame
+	void Update () {
+		transform.Rotate (new Vector3 (15, 45, 30)*Time.deltaTime,Space.Self);
+		transform.RotateAround (Vector3.zero, new Vector3(x,y,z), speed * Time.deltaTime);
+	}
+}
---
+++ Assets/RotateMoon.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RotateMoon : MonoBehaviour {
+	public int speed;
+	public int x;
+	public int y;
+	public int z;
+	public GameObject earth;
+	// Update is called once per frame
+	void Update () {
+		transform.Rotate (new Vector3 (15, 45, 30)*Time.deltaTime,Space.Self);
+		//transform.localRotation = Quaternion.identity;
+		transform.RotateAround (earth.transform.position, new Vector3(x,y,z), speed * Time.deltaTime);
+	}
+}
